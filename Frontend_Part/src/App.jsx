5a495fc609fca5f523c7c7fa0f292c0149571e47
Navneet Kushwaha <117534965+mollenmi@users.noreply.github.com>
2025-01-19 21:54:59 +0530

    Contact_formUpdate

---
+++ Frontend_Part/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Contact from './component/contact'
+
+function App() {
+  return (
+    <>
+     <Contact />
+    </>
+  )
+}
+
+export default App
