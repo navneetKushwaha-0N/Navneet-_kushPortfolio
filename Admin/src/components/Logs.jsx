336e8034e565a43baf54f559ccde13e2d7f9741c
Navneet Kushwaha <117534965+mollenmi@users.noreply.github.com>
2025-02-24 22:25:42 +0530

    logo and settingfix

--- Admin/src/components/Logs.jsx
+++ Admin/src/components/Logs.jsx
@@ -2,7 +2,7 @@ export function Logs() {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Logs/Activity</h2>
-      <p>Here you can track admin actions and user interactions.</p>
+      <p>Here you can track admin actions and user interaction.</p>
     </div>
   )
 }
--- Admin/src/components/Settings.jsx
+++ Admin/src/components/Settings.jsx
@@ -2,7 +2,7 @@ export function Settings() {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Settings</h2>
-      <p>Here you can change portfolio details like name, description, social links, and theme.</p>
+      <p>Here you can change portfolio details like name, description, social links, and themes.</p>
     </div>
   )
 }
