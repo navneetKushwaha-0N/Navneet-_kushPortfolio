a3d207c466af0caff603f643e86afc43e2b3f3d2
Navneet Kushwaha <117534965+mollenmi@users.noreply.github.com>
2025-04-18 21:33:09 +0530

    remove env file

--- Admin/src/components/Overview.jsx
+++ Admin/src/components/Overview.jsx
@@ -13,13 +13,13 @@ export function Overview() {
         <h3 className="text-lg font-semibold mb-2 flex items-center">
           <FaMousePointer className="mr-2" /> Clicks
         </h3>
-        <p className="text-3xl font-bold">567</p>
+        <p className="text-3xl font-bold">5673</p>
       </div>
       <div className="bg-white p-4 rounded-lg shadow">
         <h3 className="text-lg font-semibold mb-2 flex items-center">
           <FaPaperPlane className="mr-2" /> Form Submissions
         </h3>
-        <p className="text-3xl font-bold">89</p>
+        <p className="text-3xl font-bold">577</p>
       </div>
     </div>
   )
